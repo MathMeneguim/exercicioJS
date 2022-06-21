19a1cd0c94749a10f35b5ed2c20303ed83d33250
Matheus <matheus.meneguim@gmail.com>
2022-06-21 13:28:29 -0300

    exercicio6

--- manipulacaoArray.js
+++ manipulacaoArray.js
@@ -5,6 +5,7 @@ const lista = [ 1, 3, 9, 12, 19, 12, 20, 40, 90, 88, 77, 4, 8, 7, 3, 4, 3 ]
 console.log('\n1. Classifique a lista em ordem crescente.');
   console.log('Lista Crescente = ', lista.sort((a, b) => a - b))
 
+
 console.log('\n2. Classifique a lista em ordem decrecente.');
   console.log('Lista Decrescente = ', lista.sort((a, b) => b - a))
 
@@ -26,3 +27,12 @@ console.log('\n4. Encontre o menor valor.');
 console.log('\n5. Remova o quinto elemento da lista.');
   lista.splice(4,1);
   console.log('lista sem o quinto elemento = ',lista);
+
+
+console.log('\n6. Mova o sétimo elemento para a segunda posição.');
+  function changePosition(lista, from, to) {
+    lista.splice(to, 0, lista.splice(from, 1)[0]);
+    return lista;
+  };
+  listaMovida = changePosition(lista, 6, 1);
+  console.log('lista com elemento movido = ',listaMovida);
