76578e02f6383c3eb2fd87bbd4fec4967ed1db5b
Matheus <matheus.meneguim@gmail.com>
2022-06-21 13:29:19 -0300

    exercicio7

--- manipulacaoArray.js
+++ manipulacaoArray.js
@@ -36,3 +36,8 @@ console.log('\n6. Mova o sétimo elemento para a segunda posição.');
   };
   listaMovida = changePosition(lista, 6, 1);
   console.log('lista com elemento movido = ',listaMovida);
+
+  
+console.log('\n7. Inclua um elemento com o valor "100" na terceira posição.');
+  lista.splice(2,0, 100)
+  console.log('lista com o numero 100 = ', lista)
