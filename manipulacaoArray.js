54442fa90543f1d85a16a62e766d2c32332ad715
Matheus <matheus.meneguim@gmail.com>
2022-06-21 13:23:24 -0300

    exercicio2

--- manipulacaoArray.js
+++ manipulacaoArray.js
@@ -4,3 +4,6 @@ const lista = [ 1, 3, 9, 12, 19, 12, 20, 40, 90, 88, 77, 4, 8, 7, 3, 4, 3 ]
 
 console.log('\n1. Classifique a lista em ordem crescente.');
   console.log('Lista Crescente = ', lista.sort((a, b) => a - b))
+
+ console.log('\n2. Classifique a lista em ordem decrecente.');
+  console.log('Lista Decrescente = ', lista.sort((a, b) => b - a))
